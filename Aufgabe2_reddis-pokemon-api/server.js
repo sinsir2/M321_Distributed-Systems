c640939bfa750f725ea477f3ebd762d25037cc06
Moritz <moritz.hulliger@gmail.com>
2025-07-06 20:22:03 +0200

    fix aufgabe2

--- Aufgabe2_reddis-pokemon-api/server.js
+++ Aufgabe2_reddis-pokemon-api/server.js
@@ -2,29 +2,17 @@ import express from 'express';
 import axios from 'axios';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { createClient } from 'redis';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const redisClient = createClient({ url: 'redis://localhost:6379' });
-await redisClient.connect();
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/pokemon/:name', async (req, res) => {
   const name = req.params.name.toLowerCase();
-  const cacheKey = `pokemon:${name}`;
 
   try {
-    const cached = await redisClient.get(cacheKey);
-    if (cached) {
-      console.log('✅ Cache hit:', name);
-      return res.json(JSON.parse(cached));
-    }
-
-    console.log(' Fetching from API:', name);
     const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const lean = {
       id: data.id,
@@ -36,7 +24,6 @@ app.get('/api/pokemon/:name', async (req, res) => {
       stats: Object.fromEntries(data.stats.map(s => [s.stat.name, s.base_stat])),
     };
 
-    await redisClient.set(cacheKey, JSON.stringify(lean), { EX: 600 });
     res.json(lean);
   } catch (err) {
     res.status(500).json({ error: 'Could not fetch Pokémon.' });
